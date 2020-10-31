eacbd9f621b371ca318c83b69873e69c198d12c6
Carlos Dávila <casedaro15@gmail.com>
2020-10-31 18:48:19 +0100

    movement

---
+++ JS/enemies.js
@@ -0,0 +1,33 @@
+class Enemie {
+    constructor(ctx, gameW, gameH, enemyWidth, enemyHeight, enemySpeed) {
+        this.ctx = ctx
+        this.gameWidth = gameW
+        this.gameHeight = gameH
+        this.width = enemyWidth
+        this.height = enemyHeight
+        this.posX = undefined
+        this.posY = undefined
+        this.speed = enemySpeed
+        this.init()
+    }
+
+    init() {
+        this.posX = parseInt(Math.random() * this.gameWidth + 1)
+        this.posY = parseInt(Math.random() * this.gameHeight + 1)
+
+    }
+
+    draw() {
+        
+        this.ctx.fillStyle = 'red'
+        this.ctx.fillRect(this.posX, this.posY, this.width, this.height)
+        this.move()
+    }
+
+    move() {
+        this.posX < this.gameWidth / 2 ? this.posX += this.speed : this.posX -= this.speed;
+        this.posY < this.gameHeight / 2 ? this.posY += this.speed : this.posY -= this.speed
+
+    }
+
+}
